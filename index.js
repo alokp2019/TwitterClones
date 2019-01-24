b140e7013e243308facb2a1264fb5163b81567be
ALOK <alokp2019@gmail.com>
2019-01-25 00:03:36 +0530

    Added Files

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const http = require('http');
+const hbs = require('hbs');
+const expressHandlebars = require('express-handlebars');
+const mongoose = require('mongoose');
+const port = process.env.PORT|| 5000;
+const server = http.createServer(app);
+const publicPath = path.join(__dirname, '/public');
+
+
+app.use(express.static(publicPath))
+
+
+server.listen(port,()=> {
+  console.log(`Server running successfully at the port ${port}`);
+
+});
